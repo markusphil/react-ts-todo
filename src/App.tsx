df7db40856a902c6ea7df993244d9a475d1a1ec2
markusphil <m.philipp1991@web.de>
2020-10-29 10:54:28 +0100

    add CategoryOverview

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import TaskOverview from "./components/tasks/TaskOverview";
 import { Col, Row, Container } from "./components/styled/base";
+import CategoryOverview from "./components/category/CategoryOverview";
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <h1>TODO</h1>
       <Container>
         <Row>
-          <Col width="25%"></Col> 
+          <Col width="25%"><CategoryOverview/></Col> 
           <Col width="75%"><TaskOverview /></Col> 
         </Row>
       </Container>
--- src/components/category/CategoryOverview.tsx
+++ src/components/category/CategoryOverview.tsx
@@ -1,1 +1,80 @@
-import React from "react"
+import React, { Fragment, useEffect, useReducer, useState } from "react";
+import { Category } from "../../types/types";
+import { ActionUnion } from "../../types/helpers";
+import { apiService } from "../../services/mockedApiService";
+
+
+// concern: handle API connection and changes to task data
+
+enum Actions {
+  Add = "ADD_CATEGORY",
+  AddList = "ADD_CATEGORY_LIST",
+  Remove = "REMOVE_CATEGORY",
+  Update = "UPDATE_CATEGORY",
+  Refresh = "REPLACE_CATEGORY_LIST"
+}
+
+type CategoryActionPayloads = {
+  [Actions.Add]: Category;
+  [Actions.AddList]: Category[];
+  [Actions.Remove]: { id: number };
+  [Actions.Update]: Category;
+  [Actions.Refresh]: Category[];
+};
+
+type CategoryActions = ActionUnion<CategoryActionPayloads>;
+
+function categoryReducer(state: Category[], action: CategoryActions): Category[] {
+  switch (action.type) {
+    case Actions.Add:
+      return [...state, action.payload];
+    case Actions.AddList:
+      return [...state, ...action.payload];
+    case Actions.Remove:
+      return state.filter((c) => c.id !== action.payload.id);
+    case Actions.Update:
+      return state.map((c) =>
+        c.id === action.payload.id ? action.payload : c
+      );
+    case Actions.Refresh:
+      return action.payload
+  }
+}
+
+function CategoryOverview() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [categories, dispatch] = useReducer(categoryReducer, []);
+
+  function fetchCategories(){
+    console.log("fetch")
+    return apiService.get("category").then(res => {
+        dispatch({
+          type: Actions.Refresh,
+          payload: res,
+        });
+      })
+  }
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetchCategories().then(()=> setIsLoading(false));
+    setInterval(fetchCategories,10*1000);
+  }, []);
+
+  
+
+  return (
+    <Fragment>
+      {isLoading ? (
+        <div>...loading</div>
+      ) : (
+        <ul>
+           {categories.map(c=> <li>{c.name}</li>)} 
+        </ul>
+      )}
+      
+    </Fragment>
+  );
+}
+
+export default CategoryOverview;
