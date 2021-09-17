ea45ad604ef7b38d8aa93e2f316dfd810eca81c0
markusphil <m.philipp1991@web.de>
2021-09-17 14:40:17 +0200

    Add comments to mocked api service

--- src/services/mockedApiService.ts
+++ src/services/mockedApiService.ts
@@ -20,45 +20,45 @@ type PostReturnTypes = {
 }
 
 type ApiServiceType = {
-    get<P extends Route>(route: P , id?:number):Promise<GetReturnTypes[P]>
-    post<P extends Route>(route: P, params: PostPayloadTypes[P]):Promise<PostReturnTypes[P]>
+    get<P extends Route>(route: P, id?: number): Promise<GetReturnTypes[P]>
+    post<P extends Route>(route: P, params: PostPayloadTypes[P]): Promise<PostReturnTypes[P]>
 }
 
 interface Repo<T> {
     list: T[];
-    get: (id?:number)=>Promise<T[]>;
-    post: (params:any)=>Promise<T>;
+    get: (id?: number) => Promise<T[]>;
+    post: (params: any) => Promise<T>;
 }
 
 class TaskRepo implements Repo<Task> {
     list = [doneTask, undoneTask];
-    get (id?:number){
-        let res:Task[];
-        if(id) res = this.list.filter(t => t.id ===id);
+    get(id?: number) {
+        let res: Task[];
+        if (id) res = this.list.filter(t => t.id === id);
         res = [...this.list];
-        return new Promise<Task[]>(resolve => setTimeout(resolve.bind(null, res),5000))
+        return new Promise<Task[]>(resolve => setTimeout(resolve.bind(null, res), 5000))
     }
 
-    post(task: TaskPrototype){
-        const res = {...task, id: Math.round(Math.random()*3000) }
+    post(task: TaskPrototype) {
+        const res = { ...task, id: Math.round(Math.random() * 3000) }
         this.list.push(res);
-        return new Promise<Task>(resolve => setTimeout(resolve.bind(null, res),3000))
+        return new Promise<Task>(resolve => setTimeout(resolve.bind(null, res), 3000))
     }
 }
 
 class CategoryRepo implements Repo<Category> {
     list = [defaultCat, testCat];
-    get (id?:number){
-        let res:Category[];
-        if(id) res = this.list.filter(t => t.id ===id);
+    get(id?: number) {
+        let res: Category[];
+        if (id) res = this.list.filter(t => t.id === id);
         res = [...this.list];
-        return new Promise<Category[]>(resolve => setTimeout(resolve.bind(null, res),5000))
+        return new Promise<Category[]>(resolve => setTimeout(resolve.bind(null, res), 5000))
     }
 
-    post(task: CategoryPrototype){
-        const res = {...task, id: Math.round(Math.random()*300) }
+    post(task: CategoryPrototype) {
+        const res = { ...task, id: Math.round(Math.random() * 300) }
         this.list.push(res);
-        return new Promise<Category>(resolve => setTimeout(resolve.bind(null, res),3000))
+        return new Promise<Category>(resolve => setTimeout(resolve.bind(null, res), 3000))
     }
 }
 
@@ -67,25 +67,28 @@ const mockedCatRepo = new CategoryRepo();
 
 // TODO: get rid of ungly type assertions
 
-export const apiService:ApiServiceType = {
+/**
+ * A mocked version of an api service that provides get and set methods for a certain set of routes.
+ */
+export const apiService: ApiServiceType = {
     get: (route, id) => {
-        switch (route){
+        switch (route) {
             case "task":
                 return mockedTaskRepo.get(id) as Promise<any>;
             case "category":
                 return mockedCatRepo.get(id) as Promise<any>;
-            default :
+            default:
                 throw new Error("invalid Route!")
         };
     },
 
     post: (route, params) => {
-        switch (route){
+        switch (route) {
             case "task":
                 return mockedTaskRepo.post(params as TaskPrototype) as Promise<any>;
             case "category":
                 return mockedCatRepo.post(params as CategoryPrototype) as Promise<any>;
-            default :
+            default:
                 throw new Error("invalid Route!")
         }
     }
